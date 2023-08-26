3bd7435e93144bc393ec8c52079e0e78fe814117
Acad-9 <Acad-9@DESKTOP-QE6KT9E>
2023-08-26 09:19:17 +0800

    Add project files.

---
+++ MyfirstApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyfirstApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyfirstApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyfirstApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
